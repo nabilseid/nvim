fab8aa8871e23b729242cb1664ef419e2a5144e0
Nabil Seid <nabeelseid@gmail.com>
2024-01-03 01:02:23 +0300

    Add select ui for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,28 @@
-return { 
-  "nvim-telescope/telescope.nvim", tag = "0.1.5",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-    vim.keymap.set('n', '<C-r>', builtin.live_grep, {})
-    vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-    vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>dl', builtin.diagnostics, { noremap = true, silent = true })
-  end
+return {
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-r>', builtin.live_grep, {})
+      vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+      vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>dl', builtin.diagnostics, { noremap = true, silent = true })
+    end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
