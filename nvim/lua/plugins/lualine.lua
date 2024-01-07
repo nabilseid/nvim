9bbc033397ff9a7ceb3cfc140aa67283b4846be0
Nabil Seid <nabeelseid@gmail.com>
2024-01-07 22:01:04 +0300

    Add lualin winbar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,57 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup({})
+    require("lualine").setup({
+      options = {
+        disabled_filetypes = {
+          winbar = { "neo-tree" }, -- diable winbar in neo tree
+          statusline = {},
+        },
+      },
+
+      winbar = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 1,
+            symbols = {
+              modified = "[+]",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+              newfile = "[New]",
+            },
+          },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "buffers" },
+      },
+
+      inactive_winbar = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 1,
+            symbols = {
+              modified = "[+]",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+              newfile = "[New]",
+            },
+          },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { "buffers" },
+        lualine_z = {},
+      },
+    })
   end,
 }
