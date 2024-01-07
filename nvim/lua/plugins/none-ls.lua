2cd1e7961d279cc10f9046e0d682d3f4477f4aec
Nabil Seid <nabeelseid@gmail.com>
2024-01-07 14:14:00 +0300

    Fix snippets

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.formatting.black,
-			},
-		})
+    null_ls.setup({
+      sources = {
+        -- diagnostics
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.ruff,
 
-		vim.keymap.set("n", "<S-f>", vim.lsp.buf.format, {})
-	end,
+        -- formatting
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+      },
+    })
+
+    vim.keymap.set("n", "<S-f>", vim.lsp.buf.format, {})
+  end,
 }
