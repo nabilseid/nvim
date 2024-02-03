4e7185c60e8d9f9763b25c7750db8325a09e5af6
Nabil Seid <nabeelseid@gmail.com>
2024-02-04 00:07:52 +0300

    Update default neorg workspac

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,21 +1,22 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  -- tag = "*",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = {  -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-          },
-        },
-      },
-    })
-  end,
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	-- tag = "*",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {}, -- Loads default behaviour
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.dirman"] = { -- Manages Neorg workspaces
+					config = {
+						workspaces = {
+							africanSun = "~/repos/african-sun",
+						},
+						default_workspace = "africanSun",
+					},
+				},
+			},
+		})
+	end,
 }
