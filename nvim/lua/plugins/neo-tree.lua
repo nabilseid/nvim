c2d7d64cd4c3ba8ad9716ceef77d3c12f2acb9ef
Nabil Seid <nabeelseid@gmail.com>
2024-02-06 18:12:28 +0300

    Update neo tree to be floating by default

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,26 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      source_selector = {
-        winbar = false,
-      },
-    })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			window = {
+				position = "float",
+			},
+			source_selector = {
+				winbar = false,
+			},
+		})
 
-    vim.wo.winbar = "" -- delete winbar set previously
+		vim.wo.winbar = "" -- delete winbar set previously
 
-    vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem reveal=true position=left toggle=true<CR>")
-    vim.keymap.set("n", "<C-m>", ":Neotree position=float source=filesystem<CR>")
-    vim.keymap.set("n", "<C-g>", ":Neotree float git_status<CR>")
-  end,
+		vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem reveal=true position=left toggle=true<CR>")
+		vim.keymap.set("n", "<C-m>", ":Neotree position=float source=filesystem<CR>")
+		vim.keymap.set("n", "<C-g>", ":Neotree float git_status<CR>")
+	end,
 }
